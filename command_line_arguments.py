3a6969a3ffd3f1b6a484fcd60f16a9582197a64e
denizisik <denizkisisel1@gmail.com>
2025-11-04 20:13:44 +0300

    table backup and fix some uniformity

--- command_line_arguments.py
+++ command_line_arguments.py
@@ -9,57 +9,57 @@ class ArgumentParsing:
         self.parser.add_argument("-r",
                                  "--random",
                                  action="store_true",
-                                 help="Get a random word from the database")
+                                 help="Get a random word from the database.")
 
         self.parser.add_argument("-l",
                                  "--language",
                                  type=str,
-                                 help="Specify the language for the random word")
+                                 help="Specify the language for the random word.")
 
         self.parser.add_argument("-n",
                                  "--number",
                                  type=int,
-                                 help="Number of random words to retrieve")
+                                 help="Number of random words to retrieve.")
 
         self.parser.add_argument("-a",
                                  "--analyze",
                                  action="store_true",
-                                 help="Analyze scraped data")
+                                 help="Analyze scraped data.")
 
         self.parser.add_argument("-s",
                                  "--scrape",
                                  action="store_true",
-                                 help="Scrape data from the web")
+                                 help="Scrape data from the web.")
 
         self.parser.add_argument("-R",
                                  "--revert",
                                  action="store_true",
-                                 help="Revert to previous data")
+                                 help="Revert to previous data.")
 
         self.parser.add_argument("-b",
                                  "--backup",
                                  action="store_true",
-                                 help="Backup current data")
+                                 help="Backup current data.")
 
         self.parser.add_argument("-g",
                                  "--good",
                                  action="store_true",
-                                 help="Mark data as good")
+                                 help="Mark data as good.")
 
         self.parser.add_argument("-C",
                                  "--clean",
                                  action="store_true",
-                                 help="Clean incomplete data")
+                                 help="Clean incomplete data.")
 
         self.parser.add_argument("-c",
                                  "--confirm-clean",
                                  action="store_true",
-                                 help="Confirm and clean incomplete data")
+                                 help="Confirm and clean incomplete data.")
 
         self.parser.add_argument("-i",
                                  "--input",
                                  type=str,
-                                 help="Input file for data")
+                                 help="Input file for data.")
 
     def parse_arguments(self):
         return self.parser.parse_args()
